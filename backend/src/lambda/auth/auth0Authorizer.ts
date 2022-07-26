e31aa2cc6c13c29d82cdf45f3497659e4c57c88a
hazy254 <jumaemmanuel.EJ@outlook.com>
2022-07-26 10:31:25 +0300

    Certificate from auth0

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,10 +1,10 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
+//import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
@@ -57,7 +57,7 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   try{
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  // const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
   // We need to generate the certificate from the endpoint given
